d82d8ba28f670e8454e2293bc9d6e5569a8974b2
egghead <thomas.omans@gmail.com>
2012-08-08 09:41:48 -0700

    Adding lazy fibonacci example, magical lazy sequences

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crash-course "0.1.1-SNAPSHOT"
+(defproject crash-course "0.1.2-SNAPSHOT"
   :description "A crash course introduction to clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main crash-course.core)
--- src/crash_course/core.clj
+++ src/crash_course/core.clj
@@ -4,14 +4,34 @@
 (defn square [n]
   (* n n))
 
+(defn fib-seq
+  "A lazy sequence representing the fibonacci sequence"
+  ([seed step]
+     (cons seed
+           (lazy-seq (fib-seq step
+                              (+ seed step)))))
+  ([] (fib-seq 0N 1N)))
+
 (defn fib [n]
   (if (< n 2) n
       (+ (fib (- n 1))
          (fib (- n 2)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Fibonacci party!"
   [n]
-  (let [n (Integer/parseInt n)]
-    (println "The square of" n "is" (square n))
-    (println "The fibonacci number for" n "is" (fib n))))
+  (let [n (Integer/parseInt n)
+        fibs (fib-seq)
+        square-fibs (map square fibs)
+        odd-fibs (filter odd? fibs)
+        odd-square-fibs (filter odd? square-fibs)]
+    (println "Have our complex sequences been realized?"
+             (realized? odd-square-fibs))
+    (println "The square of" n "is"
+             (square n))
+    (println "The fibonacci numbers up to " n "are"
+             (take n fibs))
+    (println "The squares of the first" n "fibonacci numbers are"
+             (take n square-fibs))
+    (println "The sum of the first" n "odd fibonacci squares is"
+             (reduce + (take n odd-square-fibs)))))
