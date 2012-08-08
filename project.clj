022006eabd9d3b1821fa4422ded2729b853e7baa
egghead <thomas.omans@gmail.com>
2012-08-07 22:12:01 -0700

    Adding simple fibonacci example

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
-(defproject crash-course "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject crash-course "0.1.1-SNAPSHOT"
+  :description "A crash course introduction to clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main crash-course.core)
--- src/crash_course/core.clj
+++ src/crash_course/core.clj
@@ -1,7 +1,17 @@
 (ns crash-course.core
   (:gen-class))
 
+(defn square [n]
+  (* n n))
+
+(defn fib [n]
+  (if (< n 2) n
+      (+ (fib (- n 1))
+         (fib (- n 2)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [n]
+  (let [n (Integer/parseInt n)]
+    (println "The square of" n "is" (square n))
+    (println "The fibonacci number for" n "is" (fib n))))
